08856b08be15dbcc2a3d4d97b1f513f47d59a17d
Stijn Verhoeven <12001732@student.pxl.be>
2021-08-14 17:27:15 +0200

    werkt
    alleen nog inverted toevoegen

--- main.c
+++ main.c
@@ -9,42 +9,54 @@ typedef struct {
     unsigned char red;
 } pixel;
 
+void helpmenu();
 int getSizeBmp(FILE *f);
 void readInputBmpfile(pixel* RGB, int imageSize, unsigned char* bmpheader, FILE *f);
-//void fileNames(char* outputFileName, char* outputNameRed, char* outputNameGreen, char* outputNameBlue);
-void outputBMP(pixel* RGB, unsigned char* bmpheader, int imageSize);
+void outputBMP(pixel* RGB, unsigned char* bmpheader, int imageSize, char* inputFileName);
+void removeBMPfromString(char* inputFileName);
 void outputRGBRed(unsigned char* RGBRed, pixel* RGB, int imageSize);
-char outputBMPFileName(char* outputFileName, char str[]);
-int main(){
-    FILE *f = fopen("rechthoek.bmp", "rb");
+char* outputBMPFileName(char* inputFileName, char str[]);
+void createOutput(unsigned char* bmpheader, char* outputName, unsigned char* RGBout, int imageSize);
+void outputRGBGreen(unsigned char* RGBGreen, pixel* RGB, int imageSize);
+void outputRGBBlue(unsigned char* RGBBlue, pixel* RGB, int imageSize);
+
+int main(int argc, char** argv){
+    char* inputFileName = malloc(80);
+    if(argc == 2 && strcmp(argv[1], "--help") ==0 )
+    {
+        helpmenu();
+        exit(1);
+    }
+    else if (argc == 3 && strcmp(argv[1], "-i") == 0) {
+                strcpy(inputFileName, argv[2]);
+            }
+
+
+    FILE *f = fopen(inputFileName, "rb");
     if (f == NULL) {
         printf("something went wrong while opening file");
         exit(1);
     }
     int imageSize = 0;
     imageSize = getSizeBmp(f);
+
     int arraySize = imageSize/3;
     pixel* RGB = malloc(arraySize * sizeof(*RGB));
     unsigned char *bmpheader = malloc(54);
     readInputBmpfile(RGB, imageSize, bmpheader, f);
-    outputBMP(RGB, bmpheader, imageSize);
 
+    outputBMP(RGB, bmpheader, imageSize,inputFileName);
 
-
-  /*  printf("%d %d %d %d \n", bmpheader[2], bmpheader[3], bmpheader[4], bmpheader[5]);
-    printf("imagesize %d \n", imageSize);
-    printf("breedre * hoogte = %d\n", breedte*hoogte);
-    printf("breedte %d hoogte %d", breedte, hoogte); // imagesize aanpassen */
-    /*if (fp == NULL) {
-        printf("something went wrong while opening file");
-    }*/
-
-   //fwrite(bmpheader, 1, 54, fp);
-    //fwrite(RGB,sizeof(char), imageSize,fp);
     fclose(f);
-    //fclose(fp);
+    free(inputFileName);
     free(RGB);
     return 0;
+}
+void helpmenu(){
+    printf("-i met daarachter de inputfile\n ");
+
+    printf("voorbeeld\n");
+    printf("./splitchannels -i meme.bmp\n");
 }
 int getSizeBmp(FILE *f){
     unsigned char bmpheader[54];
@@ -72,21 +84,41 @@ void readInputBmpfile(pixel* RGB, int imageSize, unsigned char* bmpheader, FILE*
 
 
 }
-void outputBMP(pixel* RGB, unsigned char* bmpheader, int imageSize){
-    char *outputFileName = malloc(50);
-    printf("geef file naam in van outputfile zonder .bmp\n");
-    gets(outputFileName);
-    printf("getline werkt\n");
-    unsigned char* RGBRed = malloc(imageSize * sizeof(*RGBRed));
-    outputRGBRed(RGBRed, RGB, imageSize);
+void outputBMP(pixel* RGB, unsigned char* bmpheader, int imageSize, char* inputFileName){
+    removeBMPfromString(inputFileName);
+    char strRed[] = "_red.bmp";
+    char strGreen[] = "_green.bmp";
+    char strBlue[] = "_blue.bmp";
 
-    char str[] = "Red.bmp";
+    unsigned char* RGBout = malloc(imageSize * sizeof(*RGBout));
+    char* outputName;
 
-    char* outputName1;
+    //rood
+    outputRGBRed(RGBout, RGB, imageSize);
+    outputName = outputBMPFileName(inputFileName, strRed); //voor rood
+    createOutput(bmpheader , outputName, RGBout, imageSize ); // voor rood
 
-    outputBMPFileName(outputFileName, str);
-    free(outputFileName);
-    //createOutput();
+    //groen
+    outputRGBGreen(RGBout, RGB, imageSize);
+    outputName = outputBMPFileName(inputFileName, strGreen); //voor groen
+    createOutput(bmpheader, outputName, RGBout, imageSize);//voor groen
+
+    //blauw
+    outputRGBBlue(RGBout, RGB, imageSize);
+    outputName = outputBMPFileName(inputFileName, strBlue);
+    createOutput(bmpheader, outputName, RGBout, imageSize);
+
+    free(outputName);
+    free(RGBout);
+}
+void removeBMPfromString(char* inputFileName){
+    int a = strlen(inputFileName);
+
+    inputFileName[a-1] = '\0';
+    inputFileName[a-2] = '\0';
+    inputFileName[a-3] = '\0';
+    inputFileName[a-4] = '\0';
+
 }
 void outputRGBRed(unsigned char* RGBRed, pixel* RGB, int imageSize){
     int x = 0;
@@ -100,44 +132,56 @@ void outputRGBRed(unsigned char* RGBRed, pixel* RGB, int imageSize){
     }
 
 }
-char outputBMPFileName(char* outputFileName, char str[]){
+char* outputBMPFileName(char* inputFileName, char str[]){
 
-    int sizeName = strlen(outputFileName) + strlen(str);
+    int sizeName = strlen(inputFileName) + strlen(str);
 
     char* outputName = (char*) malloc(sizeName);
-
-    strcpy(outputName, outputFileName);
-    printf("werkt\n");
+    strcpy(outputName, inputFileName);
     strcat(outputName, str);
-    return 0;
+    return outputName;
 
 }
-/*void fileNames(char* outputFileNames, char* outputNameRed, char* outputNameGreen, char* outputNameBlue){
-    printf("geef file name in zonder .bmp\n");
 
-    gets(outputFileNames);
-    char strRed[] = "Red.bmp";
-    char strGreen[] = "Green.bmp";
-    char strBlue[] = "Blue.bmp";
-    int sizeOutput = strlen(outputFileNames) + strlen(strRed);
-    outputNameRed = (char*) malloc(sizeOutput);
-    sizeOutput = sizeOutput - strlen(strRed);
-    sizeOutput = sizeOutput + strlen(strGreen);
-    outputNameGreen = (char*) malloc(sizeOutput);
-    sizeOutput = sizeOutput - strlen(strGreen);
-    sizeOutput = sizeOutput + strlen(strBlue);
-    outputNameBlue = (char*) malloc(sizeOutput);
+void createOutput(unsigned char* bmpheader, char* outputName, unsigned char* RGBout, int imageSize){
 
-    strcpy(outputNameRed, outputFileNames);
-    strcat(outputNameRed, strRed);
-    strcpy(outputNameGreen, outputFileNames);
-    strcat(outputNameGreen, strGreen);
-    strcpy(outputNameBlue, outputFileNames);
-    strcat(outputNameBlue, strBlue);
-    printf("%s\n", outputNameRed);
-    printf("%s\n", outputNameGreen);
-    printf("%s\n", outputNameBlue);
+    FILE *fp;
+    fp = fopen(outputName, "wb");
+    if(fp == NULL){
+        printf("file does not create\n");
+        exit(0);
+    }
+    printf("file created succesfully\n");
+    fwrite(bmpheader, 1, 54, fp);
+    fwrite(RGBout, sizeof(unsigned char), imageSize, fp);
+    printf("output  finished\n");
+    fclose(fp);
 
-}*/
+
+}
+void outputRGBGreen(unsigned char* RGBGreen, pixel* RGB, int imageSize){
+    int x = 0;
+    int j = imageSize / 3;
+    for(int i = 0; i < j; i++ ){
+        RGBGreen[x] = 0;
+        RGBGreen[x+1] = 0;
+        RGBGreen[x+2] = 0;
+        RGBGreen[x+1] = RGB[i].green;
+        x = x +3;
+    }
+
+}
+void outputRGBBlue(unsigned char* RGBBlue, pixel* RGB, int imageSize){
+    int x = 0;
+    int j = imageSize / 3;
+    for(int i = 0; i < j; i++){
+        RGBBlue[x] = 0;
+        RGBBlue[x+1] = 0;
+        RGBBlue[x+2] = 0;
+        RGBBlue[x] = RGB[i].blue;
+        x = x + 3;
+    }
+}
+
 
 //https://www.includehelp.com/c-programs/c-program-to-create-a-file-file-handling-programs.aspx
